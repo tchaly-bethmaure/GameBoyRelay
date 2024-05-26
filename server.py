51c09e562b74b7853c0ca0b6e99a82ce673ecb52
Tchaly <tchaly.bethmaure@gmail.com>
2024-05-26 20:40:21 +0200

    TAG v1
    First 'working' version. To test with the community.

--- server.py
+++ server.py
@@ -1,16 +1,37 @@
 from aiohttp import web
 import socketio
 
-
-async def my_message(data):
-    print("Received ", data, " from ", sid)
-    sio.emit("message", data, room='chat_users', skip_sid=sid)
-
-sio = socketio.AsyncServer(cors_allowed_origins=['http://127.0.0.1:3000'])
+sio = socketio.AsyncServer(cors_allowed_origins="*")
 app = web.Application()
-app.add_routes([web.get('/', my_message)])
 sio.attach(app)
 
+'''async def my_message(data):
+    print("Received ", data, " from ", sid)
+    sio.emit("message", data, )'''
+
+# TODO : count room and give an id. We count joined peer 2 by 2 and assigning a room for 2 peer.
+
+@sio.event
+async def gb_com(sid, message):
+    print("[+] GameBoy Communication ", message["data"]," to room ",message['room']," received.")
+    await sio.emit('gb_com', {'data': message['data']}, room=message["room"], skip_sid=sid)
+
+@sio.event
+async def my_room_event(sid, message):
+    print("[+] Message ", message["data"]," to room ",message['room']," received.")
+    await sio.emit('my_response', {'data': message['data']}, room=message['room']) # skip sid bug ? , skip_sid=sid
+
+@sio.event
+async def join(sid, message):
+    print("[+] Room ", message["room"]," joined. For ", sid)
+    await sio.enter_room(sid, message['room'])
+    await sio.emit('my_response', {'data': 'Entered room: ' + message['room']},
+                   room=sid)
+
+@sio.event
+async def connect(sid, environ):
+    print("[+] SID ",sid," connected.")
+    await sio.emit('my_response', {'data': 'Connected', 'count': 0}, room=sid)
 
 if __name__ == '__main__':
     web.run_app(app)
